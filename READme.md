093ee06f87514b9258927eac7d0914f1b617285d
Babatunde-Ben <Babatundeben110@gmail.com>
2021-07-12 14:26:18 +0100

    added github repository link

---
+++ READme.md
@@ -0,0 +1,7 @@
+# Javascript Counter
+
+The landing page consist of a box which includes the title of the project, a number and two buttons. With the aid of Javascript programming language, the two buttons were programmed to control the increment of the number and the decrement of the number within the range of 0-50
+
+### Here are the links related to my project
+[Github]("insert link here")
+[Live Preview]("insert website here")
--- counter.js
+++ counter.js
@@ -1,14 +1,5 @@
-document.querySelector('.countup').addEventListener('click', function set() {
-  const currentnumber = parseInt(document.querySelector('.number').innerHTML);
-  if (currentnumber >= 50) {
-    const newnumber = currentnumber;
-    document.querySelector('.number').innerHTML = newnumber;
-    alert(`maximum limit reached`);
-  } else {
-    const newnumber = currentnumber + 1;
-    document.querySelector('.number').innerHTML = newnumber;
-  }
-});
+// code block for the lower "count button"
+
 document.querySelector('.countdown').addEventListener('click', function set() {
   const currentnumber = parseInt(document.querySelector('.number').innerHTML);
   if (currentnumber <= 0) {
@@ -17,6 +8,20 @@ document.querySelector('.countdown').addEventListener('click', function set() {
     alert(`minimum limit reached`);
   } else {
     const newnumber = currentnumber - 1;
+    document.querySelector('.number').innerHTML = newnumber;
+  }
+});
+
+// code block for the add "count button"
+
+document.querySelector('.countup').addEventListener('click', function set() {
+  const currentnumber = parseInt(document.querySelector('.number').innerHTML);
+  if (currentnumber >= 50) {
+    const newnumber = currentnumber;
+    document.querySelector('.number').innerHTML = newnumber;
+    alert(`maximum limit reached`);
+  } else {
+    const newnumber = currentnumber + 1;
     document.querySelector('.number').innerHTML = newnumber;
   }
 });
